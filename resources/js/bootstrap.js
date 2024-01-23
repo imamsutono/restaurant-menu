1ff90e34e990c6faf982a6b366147dc331d868d7
Imam Sutono <imamsutono>
2024-01-23 08:11:43 +0700

    feat: setup vue

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,6 +8,8 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common['Accept'] = 'application/json';
+window.axios.defaults.baseURL = 'http://127.0.0.1:8000/api';
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
---
+++ resources/js/src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Welcome admin 😊</h1>
+
+    <strong>Our Menu:</strong>
+    <ul>
+        <li>
+            <router-link :to="{name: 'category'}">Category</router-link>
+        </li>
+    </ul>
+</template>
