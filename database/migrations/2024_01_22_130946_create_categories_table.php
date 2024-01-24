187b0cebe20ef1e39055a52d6fd58cb2323dda9f
Imam Sutono <imamsutono>
2024-01-24 21:55:25 +0700

    fix(sub-category): remove soft delete to reuse deleted slug

--- database/migrations/2024_01_22_130946_create_categories_table.php
+++ database/migrations/2024_01_22_130946_create_categories_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('slug')->unique();
             $table->integer('level');
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
