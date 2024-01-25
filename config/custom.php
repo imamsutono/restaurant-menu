329089d1c490cb950d65247fd4782d0e21aeab62
Imam Sutono <imamsutono>
2024-01-25 17:55:27 +0700

    feat: add menu type config

--- config/custom.php
+++ config/custom.php
@@ -4,4 +4,5 @@ return [
     'category_level' => 0,
     'max_sub_level'  => 4,
     'is_development' => config('app.env') === 'local',
+    'menu_type'      => ['category', 'subcategory', 'item']
 ];
